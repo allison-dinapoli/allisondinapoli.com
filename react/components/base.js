86e7e3c7985e08eba58eba37aa240c0a5a58af17
Allison DiNapoli <adinapol@asu.edu>
2018-06-18 10:34:28 -0700

    updating sizes and removing extra line break

--- react/components/base.js
+++ react/components/base.js
@@ -10,11 +10,11 @@ export default class Base extends React.Component {
     }
 
     render() {
-        var linkedIn = <a href="https://www.linkedin.com/in/allison-dinapoli"><img src="./assets/img/linkedin.svg" style={{width:"40px", maxWidth: "10%"}}/></a>;
-        var email = <a href="mailto:adinapol@asu.edu"><img src="./assets/img/email.svg" style={{width:"40px", maxWidth: "10%"}}/></a>;
-        var github = <a href="https://github.com/allison-dinapoli"><img src="./assets/img/github.svg" style={{width:"40px", maxWidth: "10%"}}/></a>;
+        var linkedIn = <a href="https://www.linkedin.com/in/allison-dinapoli"><img src="./assets/img/linkedin.svg" style={{width:"40px", maxWidth: "10%", maxHeight: "10%"}}/></a>;
+        var email = <a href="mailto:adinapol@asu.edu"><img src="./assets/img/email.svg" style={{width:"40px", maxWidth: "10%", maxHeight: "10%"}}/></a>;
+        var github = <a href="https://github.com/allison-dinapoli"><img src="./assets/img/github.svg" style={{width:"40px", maxWidth: "10%", maxHeight: "10%"}}/></a>;
         var socialMedia = <div style={{textAlign: "right", fontSize: "10px"}}>{linkedIn}&nbsp;{github}&nbsp;{email}</div>;
-        var page = <div style={{fontSize: "60px"}}>{socialMedia}<br/>{this.props.content}</div>;
+        var page = <div style={{fontSize: "60px"}}>{socialMedia}{this.props.content}</div>;
         return (page); 
     }
 };
